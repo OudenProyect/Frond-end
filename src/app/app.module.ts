00659e16385bbfad470484d6120220f0c0f297b4
strom99 <alissoncc17@gmail.com>
2023-01-27 17:34:31 +0100

    arreglado slider component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './views/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
+import { SwiperComponent } from "swiper/angular";
+
+// import Swiper core and required modules
+import SwiperCore, {
+  Keyboard,
+  Scrollbar,
+  Navigation,
+  Pagination,
+} from "swiper";
+
+// install Swiper modules
+SwiperCore.use([Keyboard, Scrollbar, Navigation, Pagination]);
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  selector: "app-slider",
+  template: `<swiper
+    [slidesPerView]="5"
+    [centeredSlides]="false"
+    [slidesPerGroupSkip]=""
+    [grabCursor]="true"
+    [keyboard]="{
+      enabled: true
+    }"
+    [breakpoints]="{
+      '769': {
+        slidesPerView: 2,
+        slidesPerGroup: 2
+      }
+    }"
+    [scrollbar]="true"
+    [navigation]="true"
+    [pagination]="{
+      clickable: true
+    }"
+    class="mySwiper"
+  >
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-001.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-002.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-003.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-004.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-005.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-006.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-007.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-008.jpg"
+    /></ng-template>
+    <ng-template swiperSlide
+      ><img src="https://cdn.magloft.com/github/swiper/images/page-009.jpg"
+    /></ng-template>
+  </swiper>`,
+  styleUrls: ["./slider.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class SliderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SliderComponent {}
