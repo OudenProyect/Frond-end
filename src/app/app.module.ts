c0c011406e6564cc8d4d9b33790a891412fce5b4
TrixXii <bellatrixX738@gmail.com>
2023-01-27 16:45:45 +0100

    e

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SwiperModule } from 'swiper/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SwiperModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
