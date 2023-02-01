be64eef3d4cf2e980142e0c62cd9884b39e6c4b4
strom99 <alissoncc17@gmail.com>
2023-02-01 21:51:29 +0100

    register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { HomeComponent } from './views/home/home.component';
+import { RegisterComponent } from './views/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
