afe080dd3fdb6d307561f4247f0406c966d9a81e
TrixXii <bellatrixX738@gmail.com>
2023-02-01 17:27:42 +0100

    55

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -33,9 +33,12 @@ SwiperCore.use([Pagination, Navigation]);
           <div class="info-casaMin"><img src="assets/svg/ducha.png" class="imag-info" alt="..."><span>3 baños</span></div>
           <div class="info-casaMin"><img src="assets/svg/zona.png" class="imag-info" alt="..."><span>284 m²</span> </div>
         </div>
-        <a href="#" class=" info-contact"><i><img src="assets/svg/mail_FILL0_wght400_GRAD0_opsz48.svg"></i></a>
-        <a href="#" class=" info-contact"><i><img src="assets/svg/call_FILL1_wght400_GRAD0_opsz48.svg"></i></a>
-        <a href="#" class=" info-contact"><i><img src="assets/svg/favorite_FILL0_wght400_GRAD0_opsz48.svg"></i></a>
+        <div class="contact">
+          <a href="#" class=" info-contact on"><i class="fa fa-envelope-o"></i></a>
+          <a href="#" class=" info-contact on"><i class="fa fa-phone"></i></a>
+          <a href="#" class=" info-contact"><i class="fa fa-heart-o"></i></a>
+        </div>
+        
 
       </div>
     </div>
@@ -44,9 +47,19 @@ SwiperCore.use([Pagination, Navigation]);
     <div class="card">
       <img src="assets/imag/la-moraleja-sothevies-copia-1659264338.jpg" class="card-img-top gap-2" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h6 class="card-title">Promoción RESIDENCIAL CASABLANCA</h6>
+        <h6 class="ext-muted ">599.900 €</h6>
+        <p class="card-text"><b>Obra Nueva </b>· Casa o chalet</p>
+        <div class="d-flex justify-content-center">
+          <div class="info-casaMin"><img src="assets/svg/cama (1).png" class="imag-info" alt="..."><span>4 Habi.</span></div>
+          <div class="info-casaMin"><img src="assets/svg/ducha.png" class="imag-info" alt="..."><span>3 baños</span></div>
+          <div class="info-casaMin"><img src="assets/svg/zona.png" class="imag-info" alt="..."><span>284 m²</span> </div>
+        </div>
+        <div class="contact">
+          <a href="#" class=" info-contact on"><i class="fa fa-envelope-o"></i></a>
+          <a href="#" class=" info-contact on"><i class="fa fa-phone"></i></a>
+          <a href="#" class=" info-contact"><i class="fa fa-heart-o"></i></a>
+        </div>
       </div>
     </div>
   </ng-template>
@@ -54,9 +67,19 @@ SwiperCore.use([Pagination, Navigation]);
     <div class="card">
       <img src="assets/imag/los-naranjos-idealista-1658921238.jpg" class="card-img-top gap-2" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h6 class="card-title">Promoción RESIDENCIAL CASABLANCA</h6>
+        <h6 class="ext-muted ">599.900 €</h6>
+        <p class="card-text"><b>Obra Nueva </b>· Casa o chalet</p>
+        <div class="d-flex justify-content-center">
+          <div class="info-casaMin"><img src="assets/svg/cama (1).png" class="imag-info" alt="..."><span>4 Habi.</span></div>
+          <div class="info-casaMin"><img src="assets/svg/ducha.png" class="imag-info" alt="..."><span>3 baños</span></div>
+          <div class="info-casaMin"><img src="assets/svg/zona.png" class="imag-info" alt="..."><span>284 m²</span> </div>
+        </div>
+        <div class="contact">
+          <a href="#" class=" info-contact on"><i class="fa fa-envelope-o"></i></a>
+          <a href="#" class=" info-contact on"><i class="fa fa-phone"></i></a>
+          <a href="#" class=" info-contact"><i class="fa fa-heart-o"></i></a>
+        </div>
       </div>
     </div>
   </ng-template>
