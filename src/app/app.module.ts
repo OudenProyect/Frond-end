216d01c596630d9fcac5134b4d5670426b6a145c
TrixXii <bellatrixX738@gmail.com>
2023-02-02 15:55:18 +0100

    02/02/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { HomeComponent } from './views/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
