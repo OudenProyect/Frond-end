e53dc9f542e1ca545f7950c9ad52837da23072f9
TrixXii <bellatrixX738@gmail.com>
2023-02-03 17:18:51 +0100

    ee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeModule } from './views/home/home.module';
+import { PerfilModule } from './views/perfil/perfil.module';
 
-const routes: Routes = [ {
+const routes: Routes = [ 
+  {
   path:"",
   loadChildren: () => import('./views/home/home.module').then(m => HomeModule)
-}];
+},
+{
+  path:"perfil",
+  loadChildren: () => import('./views/perfil/perfil.module').then(m => PerfilModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/perfil/perfil.module.ts
+++ src/app/views/perfil/perfil.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { PerfilComponent } from './perfil.component';
 
+const routes: Routes = [
+  {
+    path:"",
+    component: PerfilComponent
+  }
 
-
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class PerfilModule { }
