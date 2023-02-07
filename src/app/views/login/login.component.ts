c64636398c0e79e6f0e425551f59a763729d52c4
Golden0984 <manishthakur098@hotmail.com>
2023-02-07 18:18:31 +0100

    b

---
+++ src/app/views/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEnvelope, faLock 
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  lock = faLock;
+  mail = faEnvelope;
+  // @ts-ignore
+  formLogin: FormGroup;
+
+  constructor(private formGroup: FormBuilder) {
+
+   }
+
+  ngOnInit(): void {
+    this.formLogin = this.formGroup.group({
+      usuario: ['', [
+        Validators.required
+      ]],
+      contrasena: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)
+      ]]
+    }, { updateOn: 'submit' });
+  }
+
+  submitLogin() {
+    console.log(this.formLogin);
+    if (this.formLogin.valid) {
+      // registro
+      console.log("correcto");
+    } else {
+      this.formLogin.markAllAsTouched()
+    }
+  }
+
+}
