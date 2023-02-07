b4e348744eeae174cb289d79adbbf554258ff475
TrixXii <bellatrixX738@gmail.com>
2023-02-07 16:49:19 +0100

    ee

--- src/app/views/perfil/perfil.component.ts
+++ src/app/views/perfil/perfil.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.css']
+  
 })
+
 export class PerfilComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  @Input('pp') public text:string ="";
 }
