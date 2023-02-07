11cb26b691c1993469aa07e1a3a4daf0c204e973
Golden0984 <manishthakur098@hotmail.com>
2023-02-07 15:58:06 +0100

    Login

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+  lock = faLock;
+  mail = faEnvelope;
   // @ts-ignore
   formLogin: FormGroup;
 
