996f9fae5d947d02b1050013b70faed821227682
strom99 <alissoncc17@gmail.com>
2023-02-12 14:55:21 +0100

    cambios services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactModule } from './views/contact/contact.module';
-import { AboutModule } from './views/about/about.module';
-import { HomeModule } from './views/home/home.module';
-import { LoginComponent } from './views/login/login.component';
-import { LoginModule } from './views/login/login.module';
-import { PerfilModule } from './views/perfil/perfil.module';
-import { RegisterModule } from './views/register/register.module';
+
 const routes: Routes = [ 
   {
     path: "",
-    loadChildren: () => import('./views/home/home.module').then(m => HomeModule)
+    loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule)
   },
   {
     path: "register",
-    loadChildren: () => import('./views/register/register.module').then(m => RegisterModule)
+    loadChildren: () => import('./views/register/register.module').then(m => m.RegisterModule)
   },
   {
     path:"login",
-    loadChildren: () => import('./views/login/login.module').then(m => LoginModule)
+    loadChildren: () => import('./views/login/login.module').then(m => m.LoginModule)
   },
   {
     path:"perfil",
-    loadChildren: () => import('./views/perfil/perfil.module').then(m => PerfilModule)
+    loadChildren: () => import('./views/perfil/perfil.module').then(m => m.PerfilModule)
   },
   {
     path:"contact",
-    loadChildren: () => import('./views/contact/contact.module').then(m => ContactModule)
+    loadChildren: () => import('./views/contact/contact.module').then(m => m.ContactModule)
   },
   {
     path:"about",
-    loadChildren: () => import('./views/about/about.module').then(m => AboutModule)
+    loadChildren: () => import('./views/about/about.module').then(m => m.AboutModule)
   }
 ];
 
