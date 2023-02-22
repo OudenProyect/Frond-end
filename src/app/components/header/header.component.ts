4870fc1c50ef79c24901f489f36b23213d9ee63b
Golden0984 <manishthakur098@hotmail.com>
2023-02-22 16:13:50 +0100

    heder

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SesionService } from 'src/app/services/sesion.service';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  userinfo: any = null;
 
-  constructor() { }
+  constructor(private sesions: SesionService, private router: Router) { }
 
   ngOnInit(): void {
-    
+    if (localStorage.getItem('token')) {
+      this.sesions.user(localStorage.getItem('token'))
+        .subscribe(user => {
+          this.userinfo = user;
+          console.log({ user })
+        }, error => {
+          // si hubiera un error se redirigira al login
+          this.router.navigate(['login'])
+        })
+    } else {
+      this.router.navigate(['login'])
+    }
   }
 
 }
