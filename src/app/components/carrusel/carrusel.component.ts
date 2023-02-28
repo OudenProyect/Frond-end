6f41b2538d188317a9c47c94db950e8f794d9bb1
TrixXii <bellatrixX738@gmail.com>
2023-02-28 17:28:09 +0100

    hh

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -7,7 +7,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 })
 export class CarruselComponent implements OnInit {
   @ViewChild('contenidoReco') contenidoReco!: ElementRef;
-  @ViewChild('caruCont')caruCont!: ElementRef;
+  @ViewChild('caruCont') caruCont!: ElementRef;
 
   gallery_item_size!: number;
   derecha!: HTMLElement;
@@ -20,27 +20,27 @@ export class CarruselComponent implements OnInit {
     this.derecha = this.contenidoReco.nativeElement.querySelector(".btn.next");
     this.izq = this.contenidoReco.nativeElement.querySelector(".btn.prev");
 
-    const gallery_scroller = this.caruCont.nativeElement;
-    gallery_scroller.addEventListener('scroll', this.handleScroll.bind(this));
+    // const gallery_scroller = this.caruCont.nativeElement;
+    // gallery_scroller.addEventListener('scroll', this.handleScroll.bind(this));
   }
+
+  // handleScroll(event: Event) {
+  //   const gallery_scroller = event.target as HTMLElement;
   
-  handleScroll(event: Event) {
-    const gallery_scroller = event.target as HTMLElement;
+  //   // Ocultar botón "next" al llegar al final del scroll
+  //   if (gallery_scroller.scrollLeft + gallery_scroller.offsetWidth >= gallery_scroller.scrollWidth) {
+  //     this.derecha.style.visibility = 'hidden';
+  //   } else {
+  //     this.derecha.style.visibility = 'visible';
+  //   }
   
-    // Ocultar botón "next" al llegar al final del scroll
-    if (gallery_scroller.scrollLeft + gallery_scroller.offsetWidth >= gallery_scroller.scrollWidth) {
-      this.derecha.style.visibility = 'hidden';
-    } else {
-      this.derecha.style.visibility = 'visible';
-    }
-  
-    // Ocultar botón "prev" al llegar al principio del scroll
-    if (gallery_scroller.scrollLeft <= 0) {
-      this.izq.style.visibility = 'hidden';
-    } else {
-      this.izq.style.visibility = 'visible';
-    }
-  }
+  //   // Ocultar botón "prev" al llegar al principio del scroll
+  //   if (gallery_scroller.scrollLeft <= 0) {
+  //     this.izq.style.visibility = 'hidden';
+  //   } else {
+  //     this.izq.style.visibility = 'visible';
+  //   }
+  // }
 
   scrollGallery(offset: number): void {
     const gallery_scroller = this.caruCont.nativeElement;
