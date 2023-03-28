9c0274ca93114a8e13d0c3e3d587bdc25ce1d4fc
Golden0984 <manishthakur098@hotmail.com>
2023-03-28 15:36:02 +0200

    new form-subir

--- src/app/views/form-subir/form-subir.module.ts
+++ src/app/views/form-subir/form-subir.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormSubirComponent } from './form-subir.component';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
   {
     path:"",
