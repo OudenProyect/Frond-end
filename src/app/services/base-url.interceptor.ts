2a11521bc4a604d89541767c855222c750b55928
TrixXii <bellatrixX738@gmail.com>
2023-03-30 15:54:19 +0200

    rtwt

--- src/app/services/base-url.interceptor.ts
+++ src/app/services/base-url.interceptor.ts
@@ -5,7 +5,7 @@ export class BaseUrlInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // clone the http request
         req = req.clone({
-            url: `http://ouden.local${req.url}`
+            url: `http://127.0.0.1:8000${req.url}`
         });
         // move to next HttpClient request life cycle
         return next.handle(req);
