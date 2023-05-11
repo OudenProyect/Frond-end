f12d86d83c1bbea6be316195bdb37fb0b38b6e7f
TrixXii <bellatrixX738@gmail.com>
2023-05-11 15:41:39 +0200

    tttt

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -19,28 +19,7 @@ export class CarruselComponent implements OnInit {
     this.gallery_item_size = this.contenidoReco.nativeElement.querySelector("div").clientWidth;
     this.derecha = this.contenidoReco.nativeElement.querySelector(".btn.next");
     this.izq = this.contenidoReco.nativeElement.querySelector(".btn.prev");
-
-    // const gallery_scroller = this.caruCont.nativeElement;
-    // gallery_scroller.addEventListener('scroll', this.handleScroll.bind(this));
   }
-
-  // handleScroll(event: Event) {
-  //   const gallery_scroller = event.target as HTMLElement;
-  
-  //   // Ocultar botón "next" al llegar al final del scroll
-  //   if (gallery_scroller.scrollLeft + gallery_scroller.offsetWidth >= gallery_scroller.scrollWidth) {
-  //     this.derecha.style.visibility = 'hidden';
-  //   } else {
-  //     this.derecha.style.visibility = 'visible';
-  //   }
-  
-  //   // Ocultar botón "prev" al llegar al principio del scroll
-  //   if (gallery_scroller.scrollLeft <= 0) {
-  //     this.izq.style.visibility = 'hidden';
-  //   } else {
-  //     this.izq.style.visibility = 'visible';
-  //   }
-  // }
 
   scrollGallery(offset: number): void {
     const gallery_scroller = this.caruCont.nativeElement;
--- src/app/views/home-details/home-details.component.ts
+++ src/app/views/home-details/home-details.component.ts
@@ -10,7 +10,7 @@ export class HomeDetailsComponent implements OnInit {
   sliderImages = [
     '/assets/imag/chalet-la-zabaleta-idealista-1658919505.jpg',
     'assets/imag/chalet-es-camp-de-mar-copia-1658919476.jpg',
-    'assets/imag/LIVINGKITS_casas-modernas_8_151111.jpg',
+    'assets/imag/luxuryestate-marbella-1658918744.jpeg',
     'assets/imag/LIVINGKITS_casas-modernas_8_151111.jpg',
     'assets/imag/LIVINGKITS_casas-modernas_8_151111.jpg',
 
