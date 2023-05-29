f45d2577aebe16c224d28601e587b6719337d210
TrixXii <bellatrixX738@gmail.com>
2023-05-29 16:57:56 +0200

    e

--- src/app/components/formcontact/formcontact.component.ts
+++ src/app/components/formcontact/formcontact.component.ts
@@ -8,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class FormcontactComponent implements OnInit {
 
   constructor() { }
+  nameInput!: string;
+  emailInput!: string;
+  messageInput!: string;
 
+  sendEmail() {
+    const recipient = 'beatrizmunozabellan@gmail.com';
+    const subject = `Nuevo mensaje de ${this.nameInput}`;
+    const body = `Nombre: ${this.nameInput}\nCorreo electrónico: ${this.emailInput}\nMensaje: ${this.messageInput}`;
+    const mailtoUrl = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    window.location.href = mailtoUrl;
+  }
   ngOnInit(): void {
   }
 
